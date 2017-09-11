2796c3b8fc75725431d3ce11c6a337f477eac5ac
pssharma9013 <pssharma9013@gmail.com>
2017-09-10 23:33:55 -0400

    typescript types

--- Typescript/app.ts
+++ Typescript/app.ts
@@ -1,1 +1,16 @@
 console.log("IT works!");
+
+//string
+let myName = 'Ps';
+
+//number
+let num = 27.5;
+
+//Boolean
+let hasName = false; //cannot be casted to boolean if assigned to 1 or 0 as in js
+
+//assign types
+let myAge;
+myAge = 21;
+myAge = '21'; //since we dont assign it to particular type at declaration, it is assigned to type any
+
