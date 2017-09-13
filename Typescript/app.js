d4b3dfc9c64a4dc6ba1215735f7e226bf4b900c2
pssharma9013 <Pavani Rangavajhula@cworks.local>
2017-09-13 16:54:39 -0400

    types

--- Typescript/app.js
+++ Typescript/app.js
@@ -1,2 +1,54 @@
 "use strict";
 console.log("IT works!");
+//string
+var myName = 'Ps';
+//number
+var num = 27.5;
+//Boolean
+var hasName = false; //cannot be casted to boolean if assigned to 1 or 0 as in js
+//assign types
+var myAge;
+myAge = 21;
+myAge = '21'; //since we dont assign it to particular type at declaration, it is assigned to type any
+//array
+var types = ["strings", "numbers"];
+types = [100];
+//tuples not avaliable in js
+var address = ["abc street", 99];
+//cant reverse the order either
+console.log(address);
+//enums
+var Color;
+(function (Color) {
+    Color[Color["Yellow"] = 34] = "Yellow";
+    Color[Color["Red"] = 35] = "Red";
+    Color[Color["Blue"] = 36] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Yellow;
+console.log(myColor);
+//any
+var car = "Honda";
+console.log(car);
+car = { brand: "Honda", model: "crv" };
+console.log(car);
+//functions
+function returnMyAge() {
+    return num;
+}
+console.log(returnMyAge());
+//void return type
+function sayHello() {
+    console.log("Hello");
+    //return myName;  // will error out because return type is void
+}
+//argument types
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+console.log(multiply(2, 5));
+//function as types
+var myMultiply;
+myMultiply = multiply;
+myMultiply(5, 6);
+myMultiply = sayHello;
+myMultiply();
--- Typescript/app.ts
+++ Typescript/app.ts
@@ -14,3 +14,65 @@ let myAge;
 myAge = 21;
 myAge = '21'; //since we dont assign it to particular type at declaration, it is assigned to type any
 
+//array
+let types: any = ["strings","numbers"];
+types = [100];
+
+//tuples not avaliable in js
+let address:[string,number] = ["abc street", 99];
+//cant reverse the order either
+console.log(address);
+
+//enums
+enum Color {
+Yellow = 34,
+Red,
+Blue
+}
+let myColor: Color = Color.Yellow;
+console.log(myColor);
+
+
+//any
+
+let car: any = "Honda";
+console.log(car);
+car = {brand:"Honda",model:"crv"};
+console.log(car);
+
+//functions
+
+function returnMyAge():number{    //assigning return type
+return num;
+}
+
+console.log(returnMyAge());
+
+//void return type
+function sayHello():void{
+console.log("Hello");
+//return myName;  // will error out because return type is void
+}
+
+//argument types
+
+function multiply(value1:number,value2:number):number{
+return value1*value2;
+}
+console.log(multiply(2,5));
+
+//function as types
+ 
+ let myMultiply;
+ myMultiply = multiply;
+ myMultiply(5,6);
+ myMultiply = sayHello;
+ myMultiply();
+ 
+ //assigning type to function
+ 
+ let myMul: (val1:number, val2:number)=> number;
+ myMultiply = sayHello;
+ 
+
+
